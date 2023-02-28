50bd2c37e8df9ae40f2df22565366adad6a1dce6
Jaozim420 <joao.prueza@gmail.com>
2023-02-28 16:54:50 -0300

    add(favicon)

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="shortcut icon" href="/icon.svg" />
+      </Head>
       <body className="bg-background text-white">
         <Main />
         <NextScript />
